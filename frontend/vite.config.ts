d0b6e692f600982b3f20df53d51b7e5cdcad48e6
WooSangWook <raincoat@kakao.com>
2025-03-18 22:47:16 +0900

    Feat: allowedHosts 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["ww57403.synology.me"], // Add your host here
     host: true, // 추가
     port: 5173,
   },
